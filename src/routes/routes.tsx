0ae7651651b8dad1658c86cd9f705dc421e93aa0
alaminfaraji <alaminice1617@gmail.com>
2024-02-10 15:49:22 +0600

    add layout

---
+++ src/routes/routes.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import { routeGenerator } from "../utils/routeGenerator";
+import { adminPaths } from "./admin.routes";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: routeGenerator(adminPaths),
+  },
+]);
+
+export default router;
---
+++ src/utils/sidebarItemsGenerator.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import { TSidebarItems, TUserPath } from "../Types";
+
+export const SidebarItemsGenerator = (items: TUserPath[]) => {
+  const SidebarItems = items.reduce((acc: TSidebarItems[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+
+    return acc;
+  }, []);
+
+  return SidebarItems;
+};
